f4c454fde49c3b1d4910ea9e4b3757f1e2b63f9c
showzeng <694891221@qq.com>
2015-12-10 00:35:22 +0800

    add rwork1

--- pythonrequests/rwork1.py
+++ pythonrequests/rwork1.py
@@ -1,4 +1,24 @@
-import requests
+# -*- coding:utf-8 -*-
 
-r = requests.get("http://210.35.251.243/opac/search.php")
-print r.textdchfvyhh
+import requests
+import re
+from bs4 import BeautifulSoup
+
+index_url = 'http://210.35.251.243/opac/ajax_top_lend_shelf.php'
+response = requests.get(index_url)
+
+# wtf = open('pyrequests.html','w+')
+# wtf.write(response.content)
+# wtf.close()
+
+page = response.content
+page = BeautifulSoup(response.content)
+# print type(page)
+# page = page.decode('utf-8')
+# page = page.encode('gbk')
+# print page
+page = page.div.next_sibling.next_sibling
+for spancontents in page.find_all('span'):
+	print(spancontents.get_text())
+
+
